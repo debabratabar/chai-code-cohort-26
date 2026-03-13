7775fc231a675f854a6b446f7ce2d8cc7029640d
debabratabar <debabrata.bar1999@gmail.com>
2026-03-14 02:55:58 +0530

    dynamic-list

---
+++ projects/dynamic_list_generate/main.js
@@ -0,0 +1,71 @@
+const testInputField = document.getElementById('taskInput')
+const addBtn = document.getElementById('addButton')
+const taskList = document.getElementById('task-list')
+const priorityTag = document.getElementById('priority-check')
+
+
+
+
+
+addBtn.addEventListener('click' , ()=>{
+
+
+    if ( testInputField.value.trim() == ''){
+        alert('Please add a valid task!! ☺️☺️')
+        return;
+    }
+
+    const listTag = document.createElement('li')
+    const taskField = document.createElement('h3')
+    const divBtn = document.createElement('div')
+    const deleteBtn = document.createElement('button')
+    const editbutton = document.createElement('button')
+    taskField.innerText = testInputField.value
+    deleteBtn.textContent = 'Delete'
+    editbutton.textContent= 'Edit'
+    deleteBtn.classList.add('delete')
+    editbutton.classList.add('edit')
+    listTag.appendChild(taskField)
+    divBtn.appendChild(editbutton)
+    divBtn.appendChild(deleteBtn)
+
+    listTag.appendChild(divBtn)
+
+    
+    if ( priorityTag.value == 'imp'){
+            taskList.prepend(listTag)
+    }
+    else{
+        taskList.appendChild(listTag)
+    }
+    
+    testInputField.value=''
+    priorityTag.value='Choose option: '
+
+
+    deleteBtn.addEventListener('click' , ()=>{
+        taskList.removeChild(listTag)
+    })
+
+
+    editbutton.addEventListener('click' , ()=>{
+    const inputFieldTag = document.createElement('input')
+    inputFieldTag.value = taskField.innerText
+
+    taskField.replaceWith(inputFieldTag)
+    
+    window.addEventListener('keypress' , (event) =>{
+        if ( event.key=== 'Enter') {
+            event.preventDefault();
+            const newTask = inputFieldTag.value
+            const taskField1 = document.createElement('h3')
+            taskField1.textContent = newTask
+
+            inputFieldTag.replaceWith(taskField1)
+
+        }
+    })
+    
+    })
+    
+})
